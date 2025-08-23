195831919144c661f25321dd7ae9b236ff4b03fc
mohsenmak6 <64421665+mohsenmak6@users.noreply.github.com>
2025-08-24 00:05:25 +0330

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-BOT_TOKEN = "8417638218:AAGfO3ubY0ruAVsoF9-stdUM9U7nLDvTXg4"
-ADMINS = [123661460]
-DB_PATH = "media_bot.db"
-BOT_USERNAME = "bdgfilm_bot"
-REQUIRED_CHANNELS = ["@booodgeh"]
-CACHE_TIME = 300
